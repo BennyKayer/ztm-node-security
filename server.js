1294d8d6da33c42b7ffe2efb47d42cb08be9fd84
Paweł Benkowski <pawlo97.pb@gmail.com>
2024-01-26 18:04:00 +0100

    OpenSSL selft signed certificate, runinng https locally

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs")
+const https = require("https")
 const path = require("path")
 const express = require("express")
 
@@ -9,6 +11,13 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
-app.listen(PORT, () => {
+// http and https are both build in
+// these 2 files are generated using openssl command
+// openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+// see one note for what it means
+https.createServer({
+    cert: fs.readFileSync("cert.pem"),
+    key: fs.readFileSync("key.pem"),
+}, app).listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 })
