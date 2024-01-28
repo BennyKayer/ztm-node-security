bd0de9aeb7ddb888662df093aa5ee101ebae8e78
Paweł Benkowski <pawlo97.pb@gmail.com>
2024-01-28 12:57:35 +0100

    Session cookies

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const express = require("express")
 const helmet = require('helmet')
 const passport = require("passport")
 const { Strategy } = require("passport-google-oauth20")
+const cookieSession = require("cookie-session")
 
 require("dotenv").config();
 
 const ENVS = {
     gglClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    gglClientId: process.env.GOOGLE_CLIENT_ID
+    gglClientId: process.env.GOOGLE_CLIENT_ID,
+    sessionSecret1: process.env.SESSION_SECRET_1,
+    sessionSecret2: process.env.SESSION_SECRET_2
 }
 
 const PORT = 8010
@@ -27,10 +30,25 @@ passport.use(new Strategy({
     callbackURL: "https://localhost:8010/auth/google/callback"
 }, verifyCallback));
 
+// Save the session to the cookies
+passport.serializeUser((user, done) => {
+    // Sending the whole user makes cookies big
+    // just get the id and search in deserialize
+    done(null, user.id)
+})
+
+// Read the session from the cookie
+passport.deserializeUser((id, done) => {
+    // User.findById(id) can be done here
+    done(null, id)
+})
+
 const app = express()
 
 function checkLoggedIn(req, res, next) {
-    const isLoggedIn = true;
+    console.log(`Current user id is ${req.user}`);
+    // const isLoggedIn = req.user;
+    const isLoggedIn = req.isAuthenticated() && req.user
     if (!isLoggedIn) {
         return res.status(401).json({error: "You must log in!"})
     }
@@ -40,15 +58,25 @@ function checkLoggedIn(req, res, next) {
 // Helmet goes at the top
 // it adds a ton of security headers
 app.use(helmet())
+// keys - why not just key?
+// changing it will invalidate all stuff signed with it
+// that's why setting a 2nd secret as a rotation secret is a good practice
+// sign with new but still accept the old one
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    keys: [ENVS.sessionSecret1, ENVS.sessionSecret2]
+}))
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/auth/google", passport.authenticate('google', {
-    scope: ["email", "profile"]
+    scope: ["email", "profile", "https://www.googleapis.com/auth/calendar"]
 }))
 
 app.get("/auth/google/callback", passport.authenticate('google', 
 {
-    failureRedirect: "/failure", successRedirect: "/", session: false,
+    failureRedirect: "/failure", successRedirect: "/", session: true,
 }))
 // Can handle this manually with the next param
 // app.get("/auth/google/callback", passport.authenticate('google', 
